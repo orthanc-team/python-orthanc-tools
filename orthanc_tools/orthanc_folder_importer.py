3131254984326dcc6ce8d3affbd4b01aa9604fcd
Benoit Crickboom <pro@sleig.be>
2024-08-08 15:42:45 +0200

    fix in folder importer

--- orthanc_tools/orthanc_folder_importer.py
+++ orthanc_tools/orthanc_folder_importer.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     o = None
-    if api_key is not None:
+    if args.api_key is not None:
         o=OrthancApiClient(args.url, headers={"api-key":args.api_key})
     else:
         o=OrthancApiClient(args.url, user=args.user, pwd=args.pwd)
